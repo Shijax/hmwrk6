5d27a8b4029238aa93585d46eb25cd72dd2f6e30
Matthew Jackson <mattshiloh@gmail.com>
2018-09-22 23:59:28 -0500

    styling

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const nameInput = document.getElementById('inputName');
+// Buttons
 const sortButton = document.getElementById('sortButton');
 const sortingButton = document.getElementById('sortingButton');
+
+// User Interaction Text Fields
+const nameInput = document.getElementById('inputName');
 const nameForm = document.getElementById('nameForm');
 
+// Invisibility Cloak
 const hideForm = () => {
     nameForm.style.display = 'none';
 }
 
 hideForm();
 
+// Let's BBegin Button Function
 sortingButton.addEventListener('click', () => {
         if (nameForm.style.display === "none") {
         nameForm.style.display = "block";
         }
 });
 
+// Print Strings to DOM
 const printToDom = (string, divId) => {
     document.getElementById(divId).innerHTML += string;
 };
 
-const activateExpel = () => {
-document.querySelector("body").addEventListener("click", function(event) {
-  if (event.target.classList.contains('expelButton')) {
-    const buttonIClicked = event.target;
-    const cardToExpel = buttonIClicked.parentNode.parentNode;
-    cardToExpel.remove();
-  }
-})};
 
+
+// Student Card Builder
 const cardBuilder = (name) => {
     let sortingHouse = '';
     let randomizer = Math.floor((Math.random() * 4) + 1);
@@ -43,18 +43,28 @@ const cardBuilder = (name) => {
         if (randomizer === 4) {
             sortingHouse = 'Ravenclaw'
         };
-    let domString = `<div class="w-25 m-4" style="width: 18rem;">
+    let domString = `<div class="w-25 m-4";>
         <div class="studentCard">
-            <h4 class="d-flex justify-content-center">${name}</h4>
-            <h4 class="d-flex justify-content-center ${sortingHouse}">${sortingHouse}!</h4>
-            <a href="#" id="expelButton" class="btn btn-primary d-flex justify-content-center w-50 expelButton">Expel!</a>
+            <h3 class="d-flex justify-content-center" style="padding: 10px">${name}</h3>
+            <h2 class="d-flex justify-content-center ${sortingHouse}" style="padding: 10px">${sortingHouse}!</h2>
+            <a href="#" id="expelButton" class="btn btn-danger d-flex justify-content-center w-50 expelButton">Send to Azkaban!</a>
          </div>
      </div>`;
     printToDom(domString, 'studentCards');
     activateExpel();
 }
 
+// Expel Button Function
+const activateExpel = () => {
+    document.querySelector("body").addEventListener("click", function(event) {
+      if (event.target.classList.contains('expelButton')) {
+        const buttonIClicked = event.target;
+        const cardToExpel = buttonIClicked.parentNode.parentNode;
+        cardToExpel.remove();
+    }
+})};
 
+// Sort Button Function
 sortButton.addEventListener('click', (e) => {
     e.preventDefault();
     cardBuilder(nameInput.value);
